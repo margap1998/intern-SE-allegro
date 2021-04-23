29f145299f97774ee96f798443e5922d793eb945
margap1998 <margap55@gmail.com>
2021-04-23 18:04:54 +0200

    Init

---
+++ src/test/java/com/example/internseallegro/InternSeAllegroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internseallegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternSeAllegroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
